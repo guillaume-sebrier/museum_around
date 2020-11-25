77d24d5797c9d938ee9c0e83acc6246f9a368619
Argentine Serres <argentineserres@yahoo.fr>
2020-11-25 18:35:15 +0100

    beginning

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -25,6 +25,21 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
+// Initialize the geolocate control.
+// const geolocate = new mapboxgl.GeolocateControl({
+//   positionOptions: {
+//   enableHighAccuracy: true
+//   },
+//   trackUserLocation: true
+// });
+// Add the control to the map.
+// map.addControl(geolocate);
+// // Set an event listener that fires
+// // when a geolocate event occurs.
+// geolocate.on('geolocate', function() {
+//   console.log('A geolocate event has occurred.')
+// });
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
