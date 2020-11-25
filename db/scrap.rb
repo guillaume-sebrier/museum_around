50c3ccc0ffffc2c7229f95f0cc7c995b83cfdeed
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-25 16:42:40 +0100

    seed v2

---
+++ db/scrap.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'nokogiri'
+
+def scrap_expos
+  url = "https://www.parisinfo.com/ou-sortir-a-paris/infos/guides/calendrier-expositions-paris?perPage=50"
+  # fake_site = Site.new(name: "fake")
+  html_doc = Nokogiri::HTML(open(url).read)
+  html_doc.search('.article-line').each do |element|
+    p photo = element.search('img').attribute('src').value
+    p title = element.search('h3').text.strip
+    p date = element.search('.date').text.strip
+    p place = element.search('.article-line-place').text.strip
+    p description = element.search('.article-line-content').text.strip
+    # Exhibition.new(title: title, description: description, site: fake_site )
+  end
+end
+
+scrap_expos
