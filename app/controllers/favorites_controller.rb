d22aa4de4ca974c056832e59858688afee1217d5
capucine masson <capumasson@gmail.com>
2020-11-26 12:04:18 +0100

    fin navbar

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 class FavoritesController < ApplicationController
+
   def index
     @my_favorites = current_user.favorites
+    aut
   end
 
   def new
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,5 @@
 class FriendshipsController < ApplicationController
+
   def new
   end
 
