cda651a093a4969cc9af4bb19221dd548056991e
Argentine Serres <argentineserres@yahoo.fr>
2020-11-30 15:07:12 +0100

    new positions for bar and filters

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -40,7 +40,7 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }));
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl, placeholder: 'Entrez une adresse' }));
     map.addControl(geolocate);
   }
 };
