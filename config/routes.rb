1e957af104b92ed8f1998284da908eec67fefd46
Lea Giroulet <lgiroulet@hotmail.fr>
2020-12-01 15:02:00 +0100

    favorite button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "/dashboard", to: "pages#dashboard"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/dashboard", to: "pages#dashboard"
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   resources :favorites, only: [:destroy], as: "favorite_delete"
   resources :users do
     resources :friendships, only: [:new, :create, :index]
-    # resources :favorites, only: [:index]
+    resources :favorites, only: [:index]
   end
   resources :sites, only: [:show, :edit, :update, :destroy]
   get "/fav", to: "favorites#fav", as: "favorite"
